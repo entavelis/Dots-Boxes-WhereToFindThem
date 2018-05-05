5eb96d0efea0d91e562a340b46df270e84087c0a
VglsD <vaggelis@fsu.gr>
2018-05-05 22:31:13 +0200

    small fix

--- alpha-zero-general/dotsandboxes/DnBGame.py
+++ alpha-zero-general/dotsandboxes/DnBGame.py
@@ -229,7 +229,7 @@ class DnBGame():
         #             newB = np.fliplr(newB)
         #             newPi = np.fliplr(newPi)
         #         l += [(newB, list(newPi.ravel()) )]
-        print(l)
+        # print(l)
         return l
 
     def stringRepresentation(self):
