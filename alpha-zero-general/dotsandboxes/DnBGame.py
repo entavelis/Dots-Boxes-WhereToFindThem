18c5a5e8fb61c6684fd3ac13f53a6a0ecb2a2e0d
VglsD <vaggelis@fsu.gr>
2018-05-05 17:03:23 +0200

    small fix

--- alpha-zero-general/dotsandboxes/DnBGame.py
+++ alpha-zero-general/dotsandboxes/DnBGame.py
@@ -215,7 +215,7 @@ class DnBGame():
                 l.append((temp_board,_merge(temp_pi_down,temp_pi_left)))
 
                 # flipped
-                b, pid, pil = _get_flipped = _get_flipped(temp_board, temp_pi_down, temp_pi_left)
+                b, pid, pil = _get_flipped(temp_board, temp_pi_down, temp_pi_left)
 
                 l.append((b, _merge(pid,pil)))
 
