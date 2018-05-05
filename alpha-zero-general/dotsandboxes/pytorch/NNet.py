e06150f0344560469d1a22db2429ca1aba38bec4
VglsD <vaggelis@fsu.gr>
2018-05-05 22:17:57 +0200

    small fix

--- alpha-zero-general/dotsandboxes/pytorch/NNet.py
+++ alpha-zero-general/dotsandboxes/pytorch/NNet.py
@@ -128,7 +128,7 @@ class NNetWrapper(NeuralNet):
         return torch.exp(pi).data.cpu().numpy()[0], v.data.cpu().numpy()[0]
 
     def loss_pi(self, targets, outputs):
-        return -torch.sum(targets*outputs.view(-1))/targets.size()[0]
+        return -torch.sum(targets*outputs)/targets.size()[0]
 
     def loss_v(self, targets, outputs):
         return torch.sum((targets-outputs.view(-1))**2)/targets.size()[0]
